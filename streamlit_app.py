d71d71ba26cb468dfb1390bbc02cd4e5f77651d5
sgassem2 <117294836+sgassem2@users.noreply.github.com>
2026-05-21 08:38:04 -0400

    Clean up imports in streamlit_app.py
    Removed unused import for 'when_matched' from snowflake.snowpark.functions.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 #from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
 cnx = st.connection("snowflake")
 session = cnx.session()
 
